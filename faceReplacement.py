8c6ba2775a8dc317362fa155ee4aee304babafdf
Rajat Bhageria <rajat1996@gmail.com>
2017-12-15 14:20:46 -0500

    features same size

--- faceReplacement.py
+++ faceReplacement.py
@@ -1,11 +1,12 @@
 import cv2
-import PyNet as net
+#import PyNet as net
 from proj4AdetectFace import detectFace
 from scipy.spatial import Delaunay
 from scipy.spatial import ConvexHull
 from proj2helpers import calculateAFromIdx, get_rgb
 import numpy as np
 from facialLandmark import facialLandmark
+import sys
 #from getConvexHull import getConvexHull
 
 # img1 and img2 are nxn numpy matricies
@@ -27,6 +28,8 @@ def faceReplacement(img1, img2):
     #nx2 each
     landmarksImg1 = facialLandmark(img1)
     landmarksImg2 = facialLandmark(img2)
+
+    print (landmarksImg1.shape, landmarksImg2.shape)
     
     # #getRGB image
     # img1_gray = rgb2gray(img1)
@@ -51,7 +54,6 @@ def faceReplacement(img1, img2):
     hull1 = ConvexHull(landmarksImg1)
     hull2 = ConvexHull(landmarksImg2)
 
-
     #plot the image
     #plt.imshow(img1)
     #plot the triangulation
@@ -64,35 +66,55 @@ def faceReplacement(img1, img2):
     img1Features = np.vstack((landmarksImg1,hull1.points[hull1.vertices]))
     img2Features = np.vstack((landmarksImg2,hull2.points[hull2.vertices]))
 
+    #make sure features same size
+    minPts = np.min([img1Features.shape[0],img2Features.shape[0]])
+    img1Features = img1Features[0:minPts,:]
+    img2Features = img2Features[0:minPts,:]
+
     #create delanuy triangulation
-    tri1 = Delaunay(img1Features)
-    tri2 = Delaunay(img2Features)
+    tri1 = Delaunay(img1Features).simplices
+    tri2 = Delaunay(img2Features).simplices
+
+    [numTriangles1,_,] = tri1.shape
+    [numTriangles2,_,] = tri2.shape
+
+    #print (numTriangles1, numTriangles2)
+
+    #img2 = 255 * np.ones(img_in.shape, dtype=img_in.dtype)
+
+    #do the warping for each of the triangles
+    for triangle in range(0,numTriangles):
+        srcTriangle = tri1[triangle,:]
+        destTriangle = tri2[triangle,:]
+
+        r1 = cv2.boundingRect(srcTriangle)
+        r2 = cv2.boundingRect(destTriangle)
 
     #do affine warp of the triangles
-    
+
     #find transform for eachTriangle
     simplices1 = tri1.simplices
     simplices2 = tri2.simplices
-    
+
     #allocate memory
     loAffineTransforms12 = np.zeros((len(simplices1), 2, 3)) #store affine for 1 -> 2
     loAffineTransforms21 = np.zeros((len(simplices1), 2, 3)) #store affine for 2 -> 1
     pts1 = np.zeros((3,2))
     pts2 = np.zeros((3,2))
-    
+
     #calculate transform for each triangle in both directions
     for i in range(len(simplices1)):
         pts1[0] = img1Features[simplices1[i][0]]
         pts1[1] = img1Features[simplices1[i][1]]
         pts1[2] = img1Features[simplices1[i][2]]
-        
+
         pts2[0] = img2Features[simplices2[i][0]]
         pts2[1] = img2Features[simplices2[i][1]]
         pts2[2] = img2Features[simplices2[i][2]]
-        
+
         loAffineTransforms12[i] = cv2.AffineTransform(pts1, pts2)
         loAffineTransforms21[i] = cv2.AffineTransform(pts1, pts2)
-    
+
     #warp each triangle
     for i in range(len(simplices1)):
         #get points for rect
@@ -103,10 +125,9 @@ def faceReplacement(img1, img2):
         x,y,w,h = cv2.boundingRect(pts) 
         
         
-        
-        
-   
-
     #blending if want to
     #we can call this function
     #output = cv2.seamlessClone(src, dst, mask, center, cv2.NORMAL_CLONE)
+
+
+    return [img1Warped, img2Warped]
