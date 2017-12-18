7d4e4fec8aa22d8da234a69cfa8bb596da9730da
Rajat Bhageria <rajat1996@gmail.com>
2017-12-17 21:20:53 -0500

    simplify code

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 from faceReplacementForVideo import faceReplacementForVideo
 
 def main():
-
-    video1 = "./Data/Easy/TheMartian.mp4"
-    video2 = "./Data/Easy/MarquesBrownlee.mp4"
+    video2 = "./Data/Easy/TheMartian.mp4"
+    video1 = "./Data/Easy/MarquesBrownlee.mp4"
 
     faceReplacementForVideo(video1, video2)
 
